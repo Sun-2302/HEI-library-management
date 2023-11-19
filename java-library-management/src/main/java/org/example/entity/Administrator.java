12ceca0f0e8e92fff32cf308a9195a54aefc2376
Sun-2302 <hei.dera.932@gmail.com>
2023-11-19 22:30:28 +0300

    fix: add build to pom.xml

--- java-library-management/src/main/java/org/example/entity/Administrator.java
+++ java-library-management/src/main/java/org/example/entity/Administrator.java
@@ -2,12 +2,17 @@ package org.example.entity;
 
 import lombok.*;
 
-@AllArgsConstructor
+import java.util.UUID;
+
 @NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode
 @ToString
 public class Administrator extends User{
     private String password;
+
+    public Administrator(UUID id, String name, String reference, String sex, String password) {
+        super(id, name, reference, sex);
+        this.password = password;
+    }
 }
